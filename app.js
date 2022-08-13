091c36ee92b36373a81747e3a96677409fa1a475
williameduardord <107081706+williameduardord@users.noreply.github.com>
2022-08-13 18:46:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
-let nu = prompt("ingrese la cantidad de notas a promediar");
-let num = parseInt(nu);
-let nota = 0;
-let suma=0;
-let nott=0;
-let promedio=0;
-let i = 0;
-let salir="esc";
+const cotizaciones = [{
+    "symbol": "ETHBTC",
+    "price": "0.07051000"
+}, {
+    "symbol": "LTCBTC",
+    "price": "0.00254700"
+}, {
+    "symbol": "BNBBTC",
+    "price": "0.01290300"
+}, {
+    "symbol": "BNBETH",
+    "price": "0.18300000"
+}, {
+    "symbol": "BTCUSDT",
+    "price": "23451.20000000"
+}, {
+    "symbol": "ETHUSDT",
+    "price": "1653.50000000"
+}]
 
-while (i < num && nota != salir) {
-    nota = prompt("ingrese nota " + (i+1) + "  o esc para interrumpir el calculo");
-    nott = parseInt(nota);
-    suma = suma + nott;
-    i = i + 1;
-    console.log(i);
-    promedio = suma /num;
-    console.log(suma);
+let body = '';
+
+for (let i=0; i<cotizaciones.length;i++)
+{
+    body +=`<tr><td>${cotizaciones[i].symbol}</td><td>${cotizaciones[i].price}</td></tr>`
+    console.log(" Ultima Cotizacion  " + (cotizaciones[i].symbol) + "=" + (cotizaciones[i].price));
+
 }
-if (nota != salir){
-    alert("El promedio es " + promedio);
-    console.log(promedio);
+i=0;
+const productToHTML = (dato) =>{
     
-}else if (nota = salir){
-    alert("As interrumpido el calculo"); 
+    let texto = "<tr><td>" + (cotizaciones[i].symbol) + "</td><td>" + (cotizaciones[i].price) + "</td></tr>" ;
+return texto;
+};
+
+let tabla ="";
+for ( i=0; i<cotizaciones.length; i++){
+tabla += productToHTML(cotizaciones[i]);
 }
+const data = document.getElementById("data");
+data.innerHTML = tabla;
+console.log(tabla)
